ddbaf6b3116fc2688720309f63d824ecad39ac68
Derrick Joseph <derrick4tech@gmail.com>
2013-08-21 23:26:59 +0530

    Almost In Place
    Trying to figure out Y no O/P

--- dualCode.c
+++ dualCode.c
@@ -3,25 +3,33 @@
 #include "stdlib.h"
 #include "math.h"
 
-// enum bool 
+typedef enum BOOL{FALSE,TRUE} bool;
 
 struct NODE		
 {
 	char *name;
-	struct NODE *parent, *rchild , *lchild;
+	struct NODE *rchild , *lchild;
 };
 typedef struct NODE node;
 
-node evenSTACK[' '],oddSTACK[' '], unused[' '];
-long int evenTOP = 0;
+/*node *evenSTACK, *oddSTACK, *unusedSTACK;
+/*long int evenTOP = 0;
 long int oddTOP = 0;
-long int unusedTOP = 0;
+long int unusedTOP = 0;*/
+node* unusedTOP;
+node* evenTOP;
+node* oddTOP;
+
+int MAXnameLength = 50;
+int oddCOUNT = 0;
+int evenCOUNT = 0;
+
 
 
 int largestlen =0;
 int secondLargestlen = 0;	//variable to store the length of the largest string, to avoid calling strlen again and again
 char *largest;		//variable storing the largest name
-node *hash[' '] = {}; //an array for providing open hashing, i.e. a[length] = ptr to ROOT of the Binary Search Tree containing all the names having same length
+node *hash[50] = {}; //an array for providing open hashing, i.e. a[length] = ptr to ROOT of the Binary Search Tree containing all the names having same length
 
 
 //Function to accept input
@@ -40,13 +48,17 @@ char *getLine(void) {
 		if(character == '\n') { break; }
 
 		if(++length >= maximumLength) {
-			char *newBuffer = realloc(buffer, maximumLength += sizeIncrement);
-			if(newBuffer == NULL) { 
-				free(buffer);
-				return NULL; 
-			}
-			currentPosition = newBuffer + (currentPosition - buffer);
-			buffer = newBuffer;
+			if (length > MAXnameLength)
+				{printf("\n\n *******SORRY only 50 alphabets allowed*****\n\n\tPlease repeat that last input");}
+			else{
+				char *newBuffer = realloc(buffer, maximumLength += sizeIncrement);
+				if(newBuffer == NULL) { 
+					free(buffer);
+					return NULL; 
+				}
+				currentPosition = newBuffer + (currentPosition - buffer);
+				buffer = newBuffer;
+			}	
 		}
 		*currentPosition++ = character;
 	}
@@ -63,39 +75,73 @@ void saferFree(void **pp) {
 	}
 }
 
-void PUSH(node *NewNode,node *STACK, long int *TOP){
+void PUSH(node *member, node *TOP){
 	TOP++;
-	STACK[TOP] = NewNode;
+	TOP = member;
 }
 
-void inorder(node *t, int count, node *STACK, long int *TOP)
+void inorder(node *t, size_t count, node *stackTOP)
 {
     if(t!=NULL)
     {
         if (t->lchild != NULL)
-        	inorder(t->lchild, count);	
+        	inorder(t->lchild, count, stackTOP);	
         
-        if (count > 0)
-        	PUSH(t, STACK, TOP);
-        
-        PUSH(t, unused, unusedTOP);
+        if (count > 0){
+        	PUSH(t, stackTOP);
+        	count--;
+        }	
+        else
+        	PUSH(t, unusedTOP);
         // printf("%s\n",t->name);
-        count--;
         
-        inorder(t->rchild, count);
+        
+        inorder(t->rchild, count, stackTOP);
     }
 }
 
-void createHeap(int start, node *STACK, long int TOP){
-	int i, level;
-	level = 1;
-	for (i=start; i > 1; i-=2)
-	{
-		if (hash[i] != NULL)
-			inorder(hash[i],pow(2,level),STACK, TOP);
+void makeODDheap(bool option){
+	size_t i, oddLEVEL,evenLEVEL,oddREMAINING,evenREMAINING,expectedCOUNT;
+	node *prevTOP;
 
-			
+	oddLEVEL = evenLEVEL = 1;
+	if (option)
+	{
+		for (i=secondLargestlen; i > 1; i--)
+		{
+			prevTOP = oddTOP;
+			if (hash[i] != NULL){
+				if (i%2==0)					
+					{inorder(hash[i],0,unusedTOP);}
+				else
+					{inorder(hash[i],(pow(i,oddLEVEL)+oddREMAINING),oddTOP);}	
+				oddREMAINING = oddTOP - prevTOP;
+				oddLEVEL++;				
+			}			
+		}
 	}
+	else{
+		for(i = secondLargestlen; i > 1; i--)
+		{
+			if (hash[i] != NULL){		
+				if (i%2 == 0)
+				{
+					prevTOP = evenTOP;
+					expectedCOUNT = pow(i,evenLEVEL);
+					inorder(hash[i],(expectedCOUNT+evenREMAINING),evenTOP);
+					evenREMAINING = expectedCOUNT -(evenTOP - prevTOP);
+				}
+				else
+				{
+					prevTOP = oddTOP;
+					expectedCOUNT = pow(i,oddLEVEL);
+					inorder(hash[i],(expectedCOUNT+oddREMAINING),oddTOP);
+					oddREMAINING = expectedCOUNT -(oddTOP - prevTOP);
+				}
+			}
+		}
+	}
+	
 }
 
 int BSTinsert(node *q, node *NewNode){
@@ -125,8 +171,6 @@ int BSTinsert(node *q, node *NewNode){
     	p->lchild = NewNode;
     else if(st==2)
     	p->rchild = NewNode;
-
-    NewNode->parent = p;
     return 0;
 }
 
@@ -145,6 +189,11 @@ int store(char *name){
 
 	    // The Block of code responsible for keeping track of the Largest and the second Largest names;
 	    len = strlen(name); 
+	    if(len%2 == 0)
+	    	{evenCOUNT++;}
+	    else
+	    	{oddCOUNT++;}
+
 	    if (largestlen < len){
 	    	
 	    	secondLargestlen = largestlen;
@@ -164,27 +213,58 @@ int store(char *name){
     return 0;
 }
 
-
+/*
+Hello There, Let's embark on this journey to figure out my Stupid code. I have added comments wherever necessary. 
+*/
 
 int main(int argc, char const *argv[])
 {
-	long int n,i;
+	int n,i;
 	char t;
-	
+	node* looper;
+	/*The usual input stuff..:P*/
 	printf("\nEnter The no of inputs : ");
-	n = (int)getLine();
-	// t = getchar();
-	for (i = 0; i < n; ++i)
-		store(getLine());	
-
-	if (secondLargestlen%2 != 0)
+	scanf("%d",&n);
+	t=getchar(); //fflush does not works everywhere
+	for (i = 0; i < n; i++)
+		store(getLine());	//Yeah.. Go to store() directly, getLine() is just for taking input in a safer manner
+	/*
+	If sencond largest name is of odd length and it has one or more children
+		then just create an ODD MAX heap
+	Else create ODD MAXheap and an even MAXheap along with the usual 
+	*/
+	if (secondLargestlen%2 != 0) 
 	{
 		if((hash[secondLargestlen]->lchild != NULL) || (hash[secondLargestlen]->rchild != NULL))
 		{
-			oddTOP++;
-			createHeap(secondLargestlen, oddSTACK, oddTOP ); 
+			/*
+			The reason to use a STACK to represent a tree here is that I am using SEQUENTIAL representation of Trees.
+				Refer to Page no 265 of "Data Structures using C, 2nd edition" by "Yedidyah Langsman, Aaron M Tanenbuam".
+				P.S: I know u already know it...Just being polite here, no offence.
+			*/
+			node* unusedSTACK = (node*)malloc((((50/100)*oddCOUNT)+evenCOUNT)*sizeof(node));	//Unused Part is 50% of the toatal no odd inputs added to even inputs
+			unusedTOP = unusedSTACK;	//initializing TOP
+			node* oddSTACK = (node*)malloc(sizeof(node) * oddCOUNT);
+			oddTOP = oddSTACK;
+			makeODDheap(1);
+			printf("\n\n***********ODD TREE***********\n\n");
+			for(looper = oddSTACK; looper <= oddTOP ; looper++){
+				printf("%s\n", looper->name);
+			}
+			printf("\n\n************UNUSED**********\n\n");
+			for(looper = unusedSTACK; looper <= unusedTOP ; looper++){
+				printf("%s\n", looper->name);
+			}
 		}
-	}	
-
+		else
+		{
+			node* unusedSTACK = (node*)malloc(((60/100)*n)*sizeof(node));
+			unusedTOP = unusedSTACK;
+			node* oddSTACK = (node*)malloc(sizeof(node) * oddCOUNT);
+			oddTOP = oddSTACK;
+			node* evenSTACK =  (node*)malloc(sizeof(node) * evenCOUNT);
+			makeODDheap(1);		
+		}
+	}
 	return 0;
 }
